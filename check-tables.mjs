c32847c613745d6d4f4a3165e51c70ab0e7e6b3e
Mami <chizzilauretta@gmail.com>
2026-05-15 22:48:34 +0100

    remove package-lock to force fresh install on Railway

---
+++ check-tables.mjs
@@ -0,0 +1,6 @@
+﻿import('./lib/db.js').then(({openDb}) => {
+  const db = openDb();
+  const tables = db.prepare("SELECT name FROM sqlite_master WHERE type='table'").all();
+  console.log(tables.map(t => t.name).join(', '));
+  db.close();
+}).catch(e => console.error(e.message));
